50c166c63d4471e45ce44b144de60b7dfcd3c338
Jeniffer-Caroline <jeniffer_caroline150@estudante.sesisenai.org.br>
2025-07-23 14:38:33 -0300

    Título e descriçao

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from 'react';  
+import { useState } from 'react';
 function App() {
+  const [titulo, setTitulo] = useState('');
+  const [descricao, setDescricao] = useState('');
+
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+
+    <section> 
+       
+          <form action="">
+          <h3>Novo Post</h3>
+          <label htmlFor="titulo">Título</label>
+          <input type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)} id="titulo" />
+
+          <label htmlFor="descricao">Descrição</label>
+          <input type="text" value={descricao} onChange={(e) => setDescricao(e.target.value)} id="descricao" />
+        </form>
+      </section>
   )
 }
 
