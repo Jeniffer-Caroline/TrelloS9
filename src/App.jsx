de066e554c5f47cb62d6538e25695a93a76e8d8c
Jeniffer-Caroline <jeniffer_caroline150@estudante.sesisenai.org.br>
2025-07-24 14:27:35 -0300

    URL da imagem de capa

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
 import React from 'react';  
 import { useState } from 'react';
+import './App.css';
 function App() {
   const [titulo, setTitulo] = useState('');
   const [descricao, setDescricao] = useState('');
+  const [imagem, setImagem] = useState(null);
 
 
   return (
+    <section className="container"> 
+      <form action="">
+        <h3>Novo Post</h3>
+        <label htmlFor="titulo">Título</label>
+        <input type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)} id="titulo" />
 
-    <section> 
-       
-          <form action="">
-          <h3>Novo Post</h3>
-          <label htmlFor="titulo">Título</label>
-          <input type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)} id="titulo" />
+        <label htmlFor="descricao">Descrição</label>
+        <textarea value={descricao} onChange={(e) => setDescricao(e.target.value)} id="descricao" />
 
-          <label htmlFor="descricao">Descrição</label>
-          <input type="text" value={descricao} onChange={(e) => setDescricao(e.target.value)} id="descricao" />
-        </form>
-      </section>
+        <h3>URL da imagem de capa</h3>
+        <label htmlFor="imagem">Imagem</label>
+        <input
+          value={imagem || ''}
+          type="text"
+          id="imagem"
+          onChange={(e) => setImagem(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </section>
   )
 }
 
