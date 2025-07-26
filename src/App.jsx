4232838d7768e8d593897e638034f20216d3e7dd
Jeniffer-Caroline <jeniffer_caroline150@estudante.sesisenai.org.br>
2025-07-26 13:41:03 -0300

    Validação

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';  
 import { useState } from 'react';
 import './App.css';
+import {ToastContainer, toast} from 'react-toastify';
 function App() {
   const [titulo, setTitulo] = useState('');
   const [descricao, setDescricao] = useState('');
@@ -8,16 +9,32 @@ function App() {
   const [data, setData] = useState('');
   const [categoria, setCategoria] = useState('');
 
+const handleSubmit= (e) => {
+    e.preventDefault();
+  
+  const today = new Date();
+  const selectedDate = new Date(data);
+
+  today.setHours(0, 0, 0, 0);
+  selectedDate.setHours(0, 0, 0, 0);
+
+  if (selectedDate < today) {
+    alert('A data não pode ser no passado');
+
+    return;
+  }
+  alert('Post criado com sucesso!');
+  };
 
   return (
     <section className="container"> 
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <h3>Novo Post</h3>
         <label htmlFor="titulo">Título</label>
-        <input type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)} id="titulo" />
+        <input type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)} id="titulo" required />
 
         <label htmlFor="descricao">Descrição</label>
-        <textarea value={descricao} onChange={(e) => setDescricao(e.target.value)} id="descricao" />
+        <textarea value={descricao} onChange={(e) => setDescricao(e.target.value)} id="descricao" required />
 
         <h3>URL da imagem de capa</h3>
         <label htmlFor="imagem">Imagem</label>
@@ -25,7 +42,16 @@ function App() {
           value={imagem || ''}
           type="text"
           id="imagem"
-          onChange={(e) => setImagem(e.target.value)}
+          onChange={(e) => {
+            const valor = e.target.value;
+            if (valor.startsWith('http://') || valor.startsWith('https://')) {
+              setImagem(valor);
+              toast.success('URL aceita!');
+            } else {
+              setImagem('');
+              toast.error('URL inválida!');
+            }
+          }}
         />
 
         <h3>Data de publicação</h3>
@@ -34,11 +60,12 @@ function App() {
           type="date"
           id="data"
           value={data}
-          onChange={(e) => setData(e.target.value)}
+          onChange={(e) => setData(e.target.value)} required
+  
         />
 
 
-        <select value={categoria} onChange={(e) => setCategoria(e.target.value)} name="" id="categoria">
+        <select value={categoria} onChange={(e) => setCategoria(e.target.value)} name="" id="categoria" required>
           <option value="">Selecione uma categoria</option>
           <option value="artigo">Artigo</option>
           <option value="noticia">Notícia</option>
